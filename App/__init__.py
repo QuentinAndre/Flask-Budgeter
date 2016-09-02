839293610e40fca0fd33d4ef1252dd923869c16a
Quentin ANDRE <Quentin.andre@insead.edu>
2016-09-02 16:12:44 -0600

    First Commit

--- App/__init__.py
+++ App/__init__.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 heroku = Heroku(app)
 print(app.config["SQLALCHEMY_DATABASE_URI"], file=sys.stderr)
+print(app.config.items(), file=sys.stderr)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
