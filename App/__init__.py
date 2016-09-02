aa3c65ec8ad12ddd5bd9f421a0d215e6138bd03a
Quentin ANDRE <Quentin.andre@insead.edu>
2016-09-02 15:59:30 -0600

    First Commit

--- App/__init__.py
+++ App/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_heroku import Heroku
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
+heroku = Heroku(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
