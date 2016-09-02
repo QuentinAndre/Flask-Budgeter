8aeedc3d27bfa8e78832e136ce54adfd58f2060a
Quentin ANDRE <Quentin.andre@insead.edu>
2016-09-02 16:09:50 -0600

    First Commit

--- App/__init__.py
+++ App/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_heroku import Heroku
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+import sys
 app = Flask(__name__)
 app.config.from_object('config')
 heroku = Heroku(app)
+print(app.config["SQLALCHEMY_DATABASE_URI"], file=sys.stderr)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
