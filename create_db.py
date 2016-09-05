49cf069e04a988d3aa2b4d469b79196c958ec07d
Quentin ANDRE <Quentin.andre@insead.edu>
2016-09-05 17:09:23 -0600

    First Commit

--- create_db.py
+++ create_db.py
@@ -1,8 +1,9 @@
 from App import db
 from App.views import Response
 
+
+db.create_all()
 users = Response.query.all()
 for user in users:
     db.session.delete(user)
     db.session.commit()
-db.create_all()
